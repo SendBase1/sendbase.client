69b582be66b6e5eb9f98d288ee16c5bd6f1d258f
cwilson <cwilson@snugzusa.com>
2025-11-15 21:39:38 -0700

    build fix

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -1,4 +1,4 @@
-import { useState, FormEvent } from 'react';
+import { useState, type FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './SignIn.css';
 import { Button } from '@/components/ui/button';
